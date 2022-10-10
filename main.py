b814d04a929e2dacc3dabf420dd0588d74493ac7
Bathrinarayana <78746273+bathri77@users.noreply.github.com>
2022-10-10 21:35:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+import time
+
+class InstagramBot:
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+        self.bot = webdriver.Chrome()
+
+    def login(self):
+       bot = self.bot
+       bot.get("https://instagram.com/accounts/login")
+       time.sleep(3)
+       bot.find_element_by_name("username").send_keys(self.username)
+       bot.find_element_by_name("password").send_keys(self.password)
+       time.sleep(3)
+
+    def searchHashtag(self,hashtag):
+        bot = self.bot
+        bot.get("https://www.instagram.com/explore/tags/" + hashtag)
+
+
+
+insta = InstagramBot("makertheproject07","god is great")
+insta.login()
+insta.searchHashtag("Python")
+
+
